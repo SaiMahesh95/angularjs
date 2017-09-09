2ca0239f496b3322a51c947e3520ae4097786fd1
MAHESH <MAHESH@DESKTOP-GRE0TUS>
2017-09-09 20:58:08 +0530

    created blogcomment,forum

--- Backend/src/main/java/com/collaborate/DAO/ForumDao.java
+++ Backend/src/main/java/com/collaborate/DAO/ForumDao.java
@@ -1,6 +1,16 @@
 package com.collaborate.DAO;
 
-public class ForumDao 
-{
- 
+import java.util.List;
+
+import com.collaborate.Model.Blog;
+import com.collaborate.Model.Forum;
+
+public interface ForumDao {
+
+	public boolean createForum(Forum forum);
+	public Forum getForumdetails(int forumId);
+	public List<Forum> getForum();
+	public boolean approveForum(Forum forum);
+	public boolean editForum(int forumId);
+	public boolean deleteForum(int forumId);
 }
