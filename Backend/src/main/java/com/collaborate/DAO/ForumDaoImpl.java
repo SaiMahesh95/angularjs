66f4e29440236afa2e2c0295a5080a4537a99e77
MAHESH <MAHESH@DESKTOP-GRE0TUS>
2017-09-10 11:04:34 +0530

    userdaoimpl

--- Backend/src/main/java/com/collaborate/DAO/ForumDaoImpl.java
+++ Backend/src/main/java/com/collaborate/DAO/ForumDaoImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import com.collaborate.Model.Blog;
 import com.collaborate.Model.Forum;
 
-@Repository("blogDAO")
+@Repository("forumDAO")
 public class ForumDaoImpl implements ForumDao{
 	
 	@Autowired
